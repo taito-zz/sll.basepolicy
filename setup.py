a40d65b9d1141cb4ae77f6298fab04a06e6a898c
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-11-30 23:53:31 +0200

    Release 0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='sll.basepolicy',
-    version='0.1',
+    version='0.2',
     description='Base policy for sll, slt and ll packages.',
     long_description=open("README.rst").read(),
     classifiers=[
